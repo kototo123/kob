036ab9a441f0cc88d9e2291008eed83e8b52d163
tutu ko <kototo123@163.com>
2023-11-29 21:36:59 +0800

    配置MySQL与注册登录模块(上)

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pk/")
 public class IndexController {
 
-    @RequestMapping("index/")
+    @RequestMapping("/")
     public String index() {
         return "pk/index.html";
     }
